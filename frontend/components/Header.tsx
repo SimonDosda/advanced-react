b912f1a3b266dde7c47c7689d76a04a606065749
Simon Dosda <sdosda@gmail.com>
2021-02-07 13:27:35 +0100

    start to add styles

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,16 +1,45 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  background-color: red;
+  transform: skew(-7deg);
+  a {
+    text-decoration: none;
+    color: white;
+    font-size: 3rem;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
-        <Link href="/">Slick Fits</Link>
+        <Logo>
+          <Link href="/">Ivie A.</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
