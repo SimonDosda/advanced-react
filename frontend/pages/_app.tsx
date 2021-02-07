64b9ba7813fbbb6fd7537ea73af16bf931d9a71f
Simon Dosda <sdosda@gmail.com>
2021-02-07 17:42:14 +0100

    add progress and rendering

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,14 @@
+import NProgress from 'nprogress';
 import { AppProps } from 'next/app';
+import { Router } from 'next/router';
 
 import Layout from '../components/Layout';
+
+import '../atoms/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
