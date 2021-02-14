cf408cebdf3387ceb37024b428ab50ceaf18fd6f
Simon Dosda <sdosda@gmail.com>
2021-02-14 17:48:31 +0100

    add pagination to query

--- frontend/components/Products.tsx
+++ frontend/components/Products.tsx
@@ -1,11 +1,12 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import { perPage } from '../config';
 import Product from './Product';
 
 export const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY($skip: Int, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
       id
       name
       price
@@ -26,8 +27,10 @@ const ProductsList = styled.div`
   grid-gap: 60px;
 `;
 
-export default function Products() {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+export default function Products({ page }) {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: { skip: (page - 1) * perPage, first: perPage },
+  });
   if (loading) return <p>loading...</p>;
   if (error) return <p>{error.message}</p>;
   return (
--- frontend/pages/products/index.tsx
+++ frontend/pages/products/index.tsx
@@ -4,12 +4,12 @@ import Products from '../../components/Products';
 
 export default function ProductsPage() {
   const { query } = useRouter();
-  const page = parseInt(query.page, 10) || 1;
+  const page = parseInt(query.page) || 1;
 
   return (
     <div>
       <Pagination page={page} />
-      <Products />
+      <Products page={page} />
       <Pagination page={page} />
     </div>
   );
