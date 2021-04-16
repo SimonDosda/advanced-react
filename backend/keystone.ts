837c6959e15f2f01b90f07d77e53c658d83a1ad3
Simon Dosda <sdosda@gmail.com>
2021-04-16 13:31:54 +0200

    add order models to backend

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -6,6 +6,8 @@ import {
   statelessSessions,
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
+import { Order } from './schemas/Order';
+import { OrderItem } from './schemas/OrderItem';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
 import { CartItem } from './schemas/CartItem';
@@ -56,6 +58,8 @@ export default withAuth(
       Product,
       ProductImage,
       CartItem,
+      Order,
+      OrderItem,
     }),
     extendGraphqlSchema,
     ui: {
