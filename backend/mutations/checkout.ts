27721bdaf5d3d4943e3a4d54810671b54becb53d
Simon Dosda <sdosda@gmail.com>
2021-04-23 09:24:16 +0200

    managing order on backend

--- backend/mutations/checkout.ts
+++ backend/mutations/checkout.ts
@@ -2,6 +2,7 @@ import { KeystoneContext } from '@keystone-next/types';
 import {
   CartItemCreateInput,
   OrderCreateInput,
+  OrderItemCreateInput,
 } from '../.keystone/schema-types';
 import stripeConfig from '../lib/stripe';
 import { CartItem } from '../schemas/CartItem';
@@ -33,6 +34,7 @@ export default async function checkout(
         price
         description
         photo {
+          id
           image {
             id
             publicUrlTransformed
@@ -41,13 +43,16 @@ export default async function checkout(
       }
     }`,
   });
+
   // 2. Calculate the total price for user's order
-  const cartItems = user.cart.filter(({ product }) => product);
+  const cartItems: CartItemCreateInput[] = user.cart.filter(
+    ({ product }) => product
+  );
   const amount = cartItems.reduce(
-    (total, cartItem: CartItemCreateInput) =>
-      total + cartItem.product.price * cartItem.quantity,
+    (total, cartItem) => total + cartItem.product.price * cartItem.quantity,
     0
   );
+
   // 3. Create the charge with the Stripe library
   const charge = await stripeConfig.paymentIntents
     .create({
@@ -57,10 +62,32 @@ export default async function checkout(
       payment_method: token,
     })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
       throw new Error(err.message);
     });
-  console.log(charge);
-  // 4. Convert the cartitems to orderitems
-  // 5. Create the Order and return it
+
+  // 4. Convert the cartItems to orderItems
+  const orderItems: OrderItemCreateInput[] = cartItems.map((cartItem) => ({
+    name: cartItem.product.name,
+    description: cartItem.product.description,
+    photo: { connect: { id: cartItem.product.photo.id } },
+    price: cartItem.product.price,
+    quantity: cartItem.quantity,
+  }));
+
+  // 5. Create the Order
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+      user: { connect: { id: userId } },
+    },
+  });
+
+  // 6. Clean up any old cart item
+  await context.lists.CartItem.deleteMany({
+    ids: cartItems.map(({ id }) => id),
+  });
+  return order;
 }
